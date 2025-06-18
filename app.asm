6e500a719cc7d33b090238c52991964ac5e7466a
heltonlay <heltonlay15@gmail.com>
2025-06-17 22:57:01 -0300

    float to text representation. Still need to fix roundings and create its own .asm file

--- app.asm
+++ app.asm
@@ -15,16 +15,19 @@ significand:    dd  0
 integer:        dd  0
 fraction:       dd  0
 
-biasExpTxt:        db  "biased exponent: "
-biasExpTxtLen:      equ $ - biasExpTxt
-expTxt:        db  "unbiased exponent: "
+biasExpTxt:     db  "biased exponent: "
+biasExpTxtLen:  equ $ - biasExpTxt
+expTxt:         db  "unbiased exponent: "
 expTxtLen:      equ $ - expTxt
-sigTxt:        db  "significand with J-bit: "
+sigTxt:         db  "significand with J-bit: "
 sigTxtLen:      equ $ - sigTxt
-intTxt:        db  "integer: "
+intTxt:         db  "integer: "
 intTxtLen:      equ $ - intTxt
 fracTxt:        db  "fraction: "
-fracTxtLen:      equ $ - fracTxt
+fracTxtLen:     equ $ - fracTxt
+floatToTxt:     db  "float ascii representation: "
+floatToTxtLen:  equ $ - floatToTxt
+decimalPoint:   db  "."
 
 section .text
 _main:
@@ -124,11 +127,46 @@ _main:
         jg .shiftFracLeft
     .skipFracShift:
 
-    and eax, 0x7F_FFFF
-    mov [fraction], eax
+    and eax, 0x7F_FFFF                  ;   remove exponent digits
+    mov esi, 1_000_000_000              ;   must be shifted right every iteration
+    mov edi, 0x80_0000                  ;   logical AND for adding up to final result
+    mov ecx, 0
 
+    .loopFractionAsWhole:
+        inc ecx
+        cmp ecx, 9                      ;   nine iterations. Next iteration would result in a division by 2 with remainder
+        jg .breakFractionAsWhole
+        shr esi, 1
+        shr edi, 1
+        mov ebx, eax
+        and ebx, edi
+        cmp ebx, 0
+        je .loopFractionAsWhole
+        add [fraction], esi
+        jmp .loopFractionAsWhole
+    .breakFractionAsWhole:
+        
     push fracTxtLen
     push fracTxt
+    call printText
+
+    push dword [fraction]
+    call printInt
+
+    push 1
+    push breakLine
+    call printText
+
+    ;   float representation
+    push floatToTxtLen
+    push floatToTxt
+    call printText
+
+    push dword [integer]
+    call printInt
+
+    push 1
+    push decimalPoint
     call printText
 
     push dword [fraction]
